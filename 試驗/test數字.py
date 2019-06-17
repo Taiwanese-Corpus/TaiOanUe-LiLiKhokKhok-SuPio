54d71497d91b5f8bde96809b2e4aab5a1d2ffddb
niauah <fernandochen799@gmail.com>
2019-06-17 17:30:34 +0800

    Siat python path

--- 試驗/test數字.py
+++ 試驗/test數字.py
@@ -1,9 +1,11 @@
 from unittest.case import TestCase
 
+from 數字 import 整理羅馬字
+
 
 class Sooji(TestCase):
     def test_全小寫(self):
-        self.assertEqual('十一', 'tsa̍p-it')()
+        self.assertEqual(整理羅馬字('Tsa̍p-it'), 'tsa̍p-it')
         
     def test_99以下連字符(self):
-        self.assertEqual('六十一', 'la̍k-tsa̍p-it')() 
+        self.assertEqual(整理羅馬字('la̍k tsa̍p-it'), 'la̍k-tsa̍p-it')
